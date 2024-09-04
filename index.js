668ef69d114ff43c1a7f8d04e982e7b4726db3cf
uptc20202 <109162971+uptc20202@users.noreply.github.com>
2024-09-04 10:51:41 -0500

    0.1.1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const routerApi = require('./routes');
 const {errorHandler, boomError} = require('./middlewares/error.handler');
 
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT ?? 2000;
 
 app.use(express.json());
 app.use(cors());
--- libs/postgres.js
+++ libs/postgres.js
@@ -1,5 +1,6 @@
 const { Client } = require('pg');
 
+/*
 async function getConnection(){
   const client = new Client({
     host: 'localhost',
@@ -10,6 +11,23 @@ async function getConnection(){
   });
 
   await client.connect();
+  return client;
+}*/
+
+async function getConnection(){
+  const client = new Client({
+    host: 'dpg-crc5qdbqf0us73dim530-a.virginia-postgres.render.com',
+    port: 5432,
+    user: 'lucho',
+    password: 'wy5REDFscs0F7XLepy1V3MfaO05wYJJE',
+    database: 'my_task_p13y',
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+
+  await client.connect();
+
   return client;
 }
 
