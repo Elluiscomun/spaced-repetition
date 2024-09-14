6fa8be1176ecdee1ddd1a60967b9fdd7cdd37a67
uptc20202 <109162971+uptc20202@users.noreply.github.com>
2024-09-14 09:51:19 -0500

    Update DataBase

--- libs/postgres.js
+++ libs/postgres.js
@@ -18,7 +18,7 @@ async function getConnection(){
   const client = new Client({
     host: process.env.HOST_DB,
     port: process.env.PORT_DB,
-    user: process.env.PORT,
+    user: process.env.USER_DB,
     password: process.env.PASS_DB,
     database: process.env.NAME_DB,
     ssl: {
