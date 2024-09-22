50c83a5f19a0f4fa74b288b2bbaa16c6049b7582
uptc20202 <109162971+uptc20202@users.noreply.github.com>
2024-09-22 08:06:01 -0500

    Modificación error handler

--- middlewares/error.handler.js
+++ middlewares/error.handler.js
@@ -6,8 +6,10 @@ function boomError(err, req, res, next){
   if(err.isBoom){
     const { output } = err;
     res.status(output.statusCode).json(output.payload);
+  }else{
+    next(err);
   }
-  next(err);
+
 }
 
 function errorHandler(err, req, res, next){
