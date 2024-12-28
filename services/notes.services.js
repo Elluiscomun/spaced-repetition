49fa86968ab778b22ad62ca3c69fbe08c98e7bab
elluiscomun <luismiguelsantiago@outlook.es>
2024-12-28 09:12:18 -0500

    corrección de fechas

--- services/notes.services.js
+++ services/notes.services.js
@@ -10,21 +10,22 @@ class  Notes{
     const day = new Date(date_user)
 
     let notes = [];
-    notes.push(... await this.getNotesAfterDay(nametag, day));
-    notes.push(... await this.getNotesAfterWeek(nametag, day));
+    notes.push(... await this.getNotesBeforeDay(nametag, day));
+    notes.push(... await this.getNotesBeforeWeek(nametag, day));
     notes.push(... await this.getNotesAfterMonth(nametag, day));
     notes.push(... await this.getNotesAfterYear(nametag, day));
     return notes;
   }
 
-  async getNotesAfterDay(nametag, day){
+  async getNotesBeforeDay(nametag, day){
     let currentDate  = day.toISOString().split("T")[0];
     const client = await getConnection();
+
     try{
       const rta = await client.query('SELECT message_note, create_date_user, id_note FROM public."NOTE" '
         + `WHERE nametag_user = '${nametag}' `
-        +  `AND TO_CHAR(review_date, 'YYYY-MM-DD') != '${currentDate}' `
-        +  `AND EXTRACT(DAY FROM create_date_user) = ${day.getDate()-1} `
+        +  `AND (review_date IS NULL OR TO_CHAR(review_date, 'YYYY-MM-DD') != '${currentDate}') `
+        +  `AND EXTRACT(DAY FROM create_date_user) = ${day.getUTCDate()-1} `
         +  `AND EXTRACT(MONTH FROM create_date_user) = ${day.getMonth()+1} `
         +  `AND EXTRACT(YEAR FROM create_date_user) = ${day.getFullYear()}`)
 
@@ -34,18 +35,18 @@ class  Notes{
     }
   }
 
-  async getNotesAfterWeek(nametag, day){
+  async getNotesBeforeWeek(nametag, day){
     let currentDate  = day;
-    currentDate.setDate(currentDate.getDate()-7);
+    currentDate.setDate(currentDate.getUTCDate()-7);
 
     const client = await getConnection();
     try{
       const rta = await client.query('SELECT message_note, create_date_user, id_note FROM public."NOTE" '
         + `WHERE nametag_user = '${nametag}' `
-        +  `AND TO_CHAR(review_date, 'YYYY-MM-DD') != '${currentDate.toISOString().split("T")[0]}' `
-        +  `AND EXTRACT(DAY FROM create_date_user) = ${day.getDate()} `
-        +  `AND EXTRACT(MONTH FROM create_date_user) = ${day.getMonth()+1} `
-        +  `AND EXTRACT(YEAR FROM create_date_user) = ${day.getFullYear()}`)
+        +  `AND (review_date IS NULL OR TO_CHAR(review_date, 'YYYY-MM-DD') !='${day.toISOString().split("T")[0]}') `
+        +  `AND EXTRACT(DAY FROM create_date_user) = ${currentDate.getUTCDate()} `
+        +  `AND EXTRACT(MONTH FROM create_date_user) = ${currentDate.getMonth()+1} `
+        +  `AND EXTRACT(YEAR FROM create_date_user) = ${currentDate.getFullYear()}`)
 
       return rta.rows
     }finally{
@@ -59,8 +60,8 @@ class  Notes{
     try{
       const rta = await client.query('SELECT message_note, create_date_user, id_note FROM public."NOTE" '
         + `WHERE nametag_user = '${nametag}' `
-        +  `AND TO_CHAR(review_date, 'YYYY-MM-DD') != '${currentDate}' `
-        +  `AND EXTRACT(DAY FROM create_date_user) = ${day.getDate()} `
+        +  `AND (review_date IS NULL OR TO_CHAR(review_date, 'YYYY-MM-DD') != '${currentDate}') `
+        +  `AND EXTRACT(DAY FROM create_date_user) = ${day.getUTCDate()} `
         +  `AND EXTRACT(MONTH FROM create_date_user) = ${day.getMonth()} `
         +  `AND EXTRACT(YEAR FROM create_date_user) = ${day.getFullYear()}`)
 
@@ -76,8 +77,8 @@ class  Notes{
     try{
       const rta = await client.query('SELECT message_note, create_date_user, id_note FROM public."NOTE" '
         + `WHERE nametag_user = '${nametag}' `
-        +  `AND TO_CHAR(review_date, 'YYYY-MM-DD') != '${currentDate}' `
-        +  `AND EXTRACT(DAY FROM create_date_user) = ${day.getDate()} `
+        +  `AND (review_date IS NULL OR TO_CHAR(review_date, 'YYYY-MM-DD') != '${currentDate}') `
+        +  `AND EXTRACT(DAY FROM create_date_user) = ${day.getUTCDate()} `
         +  `AND EXTRACT(MONTH FROM create_date_user) = ${day.getMonth()+1} `
         +  `AND EXTRACT(YEAR FROM create_date_user) = ${day.getFullYear()-1}`);
       return rta.rows;
